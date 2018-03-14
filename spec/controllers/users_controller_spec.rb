28cb38bbf5aadbc9cd8be9e022a0049a150b18b3
brunaclarissa <37226107+brunaclarissa@users.noreply.github.com>
2018-03-14 17:31:29 -0300

    Update users_controller_spec.rb

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe UsersController, type: :controller do
 
   let(:valid_session) { {} }
 
-  describe "GET #index" do
+  describe "GET index" do
     it "returns a success response" do
       user = User.create(name: "Orcestra", email: "projetos@orcestra.com.br")
       get :index, params: {}, session: valid_session
@@ -17,22 +17,22 @@ RSpec.describe UsersController, type: :controller do
     end
   end
 
-  describe "POST #create" do
+  describe "POST create" do
     context "with valid params" do
       it "creates a new User" do
-        # expect {
-        #   post :create, params: {user: valid_attributes}
-        # }.to change(User, :count).by(2)
-        expect(nil).to be_success
+        expect {
+          post :create, params: {user: valid_attributes}
+         }.to change(User, :count).by(2)
+        # expect(nil).to be_success
       end
 
       it "renders a JSON response with the new user" do
 
         post :create, params: {user: valid_attributes}, session: valid_session
-        # expect(response).to have_http_status(:created)
+        expect(response).to have_http_status(:created)
         # expect(response.content_type).to eq('application/json')
         # expect(response.location).to eq(user_url(User.last))
-        expect(nil).to be_success
+        #expect(nil).to be_success
       end
     end
   end
