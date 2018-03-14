48b104c2ba306e8010386cf123a92e227e6de6e1
brunaclarissa <bruna.guimaraes@faciplac.edu.br>
2018-03-14 17:42:54 -0300

    Correcao do arquivo travis 1.1

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe UsersController, type: :controller do
       user = User.create(name: "Orcestra", email: "projetos@orcestra.com.br")
       get :index, params: {}, session: valid_session
        expect(response).to be_success
-#      expect(nil).to be_success
+       #expect(nil).to be_success
     end
   end
 
@@ -30,7 +30,7 @@ RSpec.describe UsersController, type: :controller do
 
         post :create, params: {user: valid_attributes}, session: valid_session
         expect(response).to have_http_status(:created)
-         expect(response.content_type).to eq('application/json')
+        expect(response.content_type).to eq('application/json')
         expect(response.location).to eq(user_url(User.last))
         #expect(nil).to be_success
       end
